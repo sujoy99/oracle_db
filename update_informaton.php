88f61653031af08da26d34c9ae257ca7ffa1959f
sujoy99 <48888636+sujoy99@users.noreply.github.com>
2020-10-19 02:39:47 +0600

    Create update_informaton.php
    commit(19-10-2020)

---
+++ update_informaton.php
@@ -0,0 +1,304 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css"
+        integrity="sha512-kJ30H6g4NGhWopgdseRb8wTsyllFUYIx3hiUwmGAkgA9B/JbzUBDQVr2VVlWGde6sdBVOG7oU8AL35ORDuMm8g=="
+        crossorigin="anonymous" />
+
+    <title>Hello, world!</title>
+
+    <style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-size: 1.6rem;
+    }
+
+    html {
+        font-size: 62.5%;
+    }
+
+    .container-fluid {
+        width: 100%;
+        height: 100vh;
+        background: rgb(222, 230, 231);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .forms {
+        height: 45rem;
+        width: 30rem;
+        background-color: #fff;
+        border-radius: 1.5rem;
+    }
+    </style>
+</head>
+
+<body>
+
+
+    <div class="container-fluid">
+        <form action="">
+            <div class="row">
+
+                <div class="card" style="width:800px; height: 100vh;">
+                    <img class="card-img-top text-center" src="#" alt="Card image">
+                    <div class="card-body">
+                        <div class="row">
+
+
+                            <div class="form-group col-lg-6">
+                                <div>
+
+
+                                    <label for="name"><b>Name : </b></label>
+
+
+                                    <label for="" id='name_lbl'>NAME</label>
+                                    <a href="javascript:void(0)"><i class="fas fa-edit float-right"
+                                            id='name_click'></i></a>
+                                    <input type="text" class="form-control col-lg-6" style='display:none;'
+                                        placeholder="Enter Name" id="name_input">
+
+                                </div>
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Nick Name : </b></label>
+                                <label for="" id='nick_name_lbl'>NICK NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;'
+                                    placeholder="Enter Nick Name" id="id='nick_name_input'">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Invitation Name : </b></label>
+                                <label for="" id='invitation_name_lbl'>INVITATION NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;'
+                                    placeholder="Enter Invitation Name" id="id='invitation_name_input">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>School Name : </b></label>
+                                <label for="">SCHOOL NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>School District : </b></label>
+                                <label for="">SCHOOL DISTRICT</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>SSC Pass Year : </b></label>
+                                <label for="">SSC PASS YEAR</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>College Name : </b></label>
+                                <label for="">COLLEGE NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>HSC Pass Year : </b></label>
+                                <label for="">HSC PASS YEAR</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Proffesion : </b></label>
+                                <label for="">PROFFESION</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Working Place : </b></label>
+                                <label for="">WORKING PLACE</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Office Address : </b></label>
+                                <label for="">OFFICE ADDRESS</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Spouce Name : </b></label>
+                                <label for="">SPOUCE NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Spouse Job : </b></label>
+                                <label for="">SPOUCE JOB</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Spouse Blood Group : </b></label>
+                                <label for="">SPOUSE BLOOD GROUP</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Baby Name : </b></label>
+                                <label for="">BABY NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Baby Name : </b></label>
+                                <label for="">BABY NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Baby Name : </b></label>
+                                <label for="">BABY NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Baby Name : </b></label>
+                                <label for="">BABY NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Baby Name : </b></label>
+                                <label for="">BABY NAME</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Email : </b></label>
+                                <label for="">EMAIL</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Phone : </b></label>
+                                <label for="">PHONE</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Blood Group : </b></label>
+                                <label for="">BLOOD GROUP</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Emergency Phone : </b></label>
+                                <label for="">EMERGENCY PHONE</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="name"><b>Remark : </b></label>
+                                <label for="">REMARK</label>
+                                <i class="fas fa-edit float-right"></i>
+                                <input type="text" class="form-control" style='display:none;' placeholder="Enter email"
+                                    id="email">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </form>
+    </div>
+
+    <!-- <table>
+			<tr>
+				<td>Name:</td>
+				<td>Name</td>
+				<td><i class="fas fa-edit"></i></td>
+			</tr>
+		</table> -->
+
+
+
+    <script>
+				var t = '0';
+				document.getElementById("name_click").addEventListener("click", ()=>{
+					if(t === '0'){
+
+						document.getElementById('name_lbl').style.display = 'none';
+						document.getElementById('name_input').style.display = 'block';
+						t = '1';
+					}
+					else{
+						document.getElementById('name_lbl').style.display = 'block';
+						document.getElementById('name_input').style.display = 'none';
+						t = '0';
+					}
+			});
+		
+		</script>
+
+
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+
+<!-- <script>
+    $(document).ready(function() {
+        $("name_click").click(function() {
+            $("name_input").toggle();
+        });
+    });
+    </script> -->
+</body>
+
+</html>
